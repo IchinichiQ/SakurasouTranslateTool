9403769dc4c84f5680620ccaa996f3995b7c38f8
12135555 <PechenkinPavelV@yandex.ru>
2020-07-21 17:51:41 +0300

    Добавьте файлы проекта.

---
+++ SakurasouTranslateTool/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SakurasouTranslateTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.dataGridViewStrings = new System.Windows.Forms.DataGridView();
+            this.buttonPatch = new System.Windows.Forms.Button();
+            this.contextMenuStripStrings = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.itemExport = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemImport = new System.Windows.Forms.ToolStripMenuItem();
+            this.ColumnPointer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnText = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelCurrentFile = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStrings)).BeginInit();
+            this.contextMenuStripStrings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonExtract.Location = new System.Drawing.Point(7, 494);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(96, 36);
+            this.buttonExtract.TabIndex = 0;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = true;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // dataGridViewStrings
+            // 
+            this.dataGridViewStrings.AllowUserToAddRows = false;
+            this.dataGridViewStrings.AllowUserToDeleteRows = false;
+            this.dataGridViewStrings.AllowUserToResizeRows = false;
+            this.dataGridViewStrings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewStrings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStrings.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnPointer,
+            this.ColumnText});
+            this.dataGridViewStrings.ContextMenuStrip = this.contextMenuStripStrings;
+            this.dataGridViewStrings.Location = new System.Drawing.Point(1, 0);
+            this.dataGridViewStrings.Name = "dataGridViewStrings";
+            this.dataGridViewStrings.RowHeadersVisible = false;
+            this.dataGridViewStrings.Size = new System.Drawing.Size(557, 488);
+            this.dataGridViewStrings.TabIndex = 1;
+            // 
+            // buttonPatch
+            // 
+            this.buttonPatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPatch.Enabled = false;
+            this.buttonPatch.Location = new System.Drawing.Point(106, 494);
+            this.buttonPatch.Name = "buttonPatch";
+            this.buttonPatch.Size = new System.Drawing.Size(96, 36);
+            this.buttonPatch.TabIndex = 2;
+            this.buttonPatch.Text = "Patch";
+            this.buttonPatch.UseVisualStyleBackColor = true;
+            this.buttonPatch.Click += new System.EventHandler(this.buttonPatch_Click);
+            // 
+            // contextMenuStripStrings
+            // 
+            this.contextMenuStripStrings.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemExport,
+            this.itemImport});
+            this.contextMenuStripStrings.Name = "contextMenuStripStrings";
+            this.contextMenuStripStrings.Size = new System.Drawing.Size(158, 48);
+            // 
+            // itemExport
+            // 
+            this.itemExport.Name = "itemExport";
+            this.itemExport.Size = new System.Drawing.Size(157, 22);
+            this.itemExport.Text = "Export strings...";
+            this.itemExport.Click += new System.EventHandler(this.itemExport_Click);
+            // 
+            // itemImport
+            // 
+            this.itemImport.Name = "itemImport";
+            this.itemImport.Size = new System.Drawing.Size(157, 22);
+            this.itemImport.Text = "Import strings...";
+            this.itemImport.Click += new System.EventHandler(this.itemImport_Click);
+            // 
+            // ColumnPointer
+            // 
+            this.ColumnPointer.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.ColumnPointer.FillWeight = 101.5228F;
+            this.ColumnPointer.HeaderText = "Pointer offset";
+            this.ColumnPointer.Name = "ColumnPointer";
+            this.ColumnPointer.ReadOnly = true;
+            this.ColumnPointer.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.ColumnPointer.Width = 80;
+            // 
+            // ColumnText
+            // 
+            this.ColumnText.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColumnText.FillWeight = 98.47716F;
+            this.ColumnText.HeaderText = "Text";
+            this.ColumnText.Name = "ColumnText";
+            this.ColumnText.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // labelCurrentFile
+            // 
+            this.labelCurrentFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCurrentFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCurrentFile.Location = new System.Drawing.Point(215, 497);
+            this.labelCurrentFile.Name = "labelCurrentFile";
+            this.labelCurrentFile.Size = new System.Drawing.Size(332, 30);
+            this.labelCurrentFile.TabIndex = 4;
+            this.labelCurrentFile.Text = "None";
+            this.labelCurrentFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 536);
+            this.Controls.Add(this.labelCurrentFile);
+            this.Controls.Add(this.buttonPatch);
+            this.Controls.Add(this.dataGridViewStrings);
+            this.Controls.Add(this.buttonExtract);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "STT";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStrings)).EndInit();
+            this.contextMenuStripStrings.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.DataGridView dataGridViewStrings;
+        private System.Windows.Forms.Button buttonPatch;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripStrings;
+        private System.Windows.Forms.ToolStripMenuItem itemExport;
+        private System.Windows.Forms.ToolStripMenuItem itemImport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPointer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnText;
+        private System.Windows.Forms.Label labelCurrentFile;
+    }
+}
+
---
+++ SakurasouTranslateTool/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SakurasouTranslateTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filename = "";
+
+        private void buttonExtract_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myDialog = new OpenFileDialog();
+
+            myDialog.Filter = "Ssb files (*.ssb)|*.ssb|All files (*.*)|*.*";
+            myDialog.RestoreDirectory = true;
+
+            if (myDialog.ShowDialog() == DialogResult.OK)
+                filename = myDialog.FileName;
+            else
+                return;
+
+            using (BinaryReader myReader = new BinaryReader(File.Open(filename, FileMode.Open)))
+            {
+                int textOffset = -1;
+                for (int i = 0; i < myReader.BaseStream.Length; i += 4)
+                {
+                    myReader.BaseStream.Seek(i, SeekOrigin.Begin);
+                    byte[] chunk = myReader.ReadBytes(35);
+                    byte[] sample = { 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D };
+                    if (chunk.SequenceEqual(sample))
+                    {
+                        textOffset = i;
+                        break;
+                    }                   
+                }
+
+                if (textOffset == -1)
+                {
+                    MessageBox.Show("Text not found!", "STT", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                dataGridViewStrings.Rows.Clear();
+
+                for (int i = 0; i < myReader.BaseStream.Length; i += 4)
+                {
+                    myReader.BaseStream.Seek(i, SeekOrigin.Begin);
+                    byte[] chunk = myReader.ReadBytes(4);
+                    int pointer = BitConverter.ToInt32(chunk, 0) * 4;
+
+                    List<byte> stringBytes = new List<byte>();
+                    if (pointer > textOffset)
+                    {
+                        for (int ii = pointer; ii < myReader.BaseStream.Length; ii++)
+                        {
+                            myReader.BaseStream.Seek(ii, SeekOrigin.Begin);
+                            byte charByte = myReader.ReadByte();
+                            if (charByte == 0x00)
+                                break;
+                            stringBytes.Add(charByte);
+                        }
+
+                        if (stringBytes.Count == 0)
+                            continue;
+
+                        string myString = Encoding.UTF8.GetString(stringBytes.ToArray());
+                        if (myString.StartsWith(" START : ") | myString.StartsWith("V_"))
+                            continue;
+                        if (myString.StartsWith(" END   : ") | myString.StartsWith("\n★★★\n★★★"))
+                            break;
+
+                        dataGridViewStrings.Rows.Add(i.ToString(), myString.Replace("\n","__"));
+                    }                 
+                }
+            }
+
+            labelCurrentFile.Text = Path.GetFileName(filename);
+            buttonPatch.Enabled = true;
+        }
+
+        private void buttonPatch_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+                return;
+
+            using (BinaryWriter myWriter = new BinaryWriter(File.Open(filename, FileMode.Open)))
+            {
+                for (int i = 0; i < dataGridViewStrings.Rows.Count; i++)
+                {
+                    int pointerOffset = Int32.Parse(dataGridViewStrings.Rows[i].Cells[0].Value?.ToString());
+                    byte[] newPointer = BitConverter.GetBytes(Convert.ToUInt32(myWriter.BaseStream.Length / 4));
+                    myWriter.Seek(pointerOffset, SeekOrigin.Begin);
+                    myWriter.Write(newPointer, 0, newPointer.Length);
+
+                    string newText = dataGridViewStrings.Rows[i].Cells[1].Value?.ToString().Replace("__", "\n");
+                    myWriter.Seek(0, SeekOrigin.End);
+                    myWriter.Write(Encoding.UTF8.GetBytes(newText));
+
+                    int nullCount = 4 - (Convert.ToInt32(myWriter.BaseStream.Length) % 4);
+                    byte[] nulls = new byte[] {0x00, 0x00, 0x00, 0x00 };
+                    myWriter.Write(nulls, 0, nullCount);
+
+                    myWriter.Flush();
+                }
+            }
+            MessageBox.Show("Done", "STT", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void itemExport_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+                return;
+
+            using (SaveFileDialog mySaveFileDialog = new SaveFileDialog())
+            {
+                mySaveFileDialog.Filter = "Text file (*.txt) | *.txt";
+
+                if (mySaveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] myStrings = new string[dataGridViewStrings.Rows.Count];
+                    for (int i = 0; i < myStrings.Length; i++)
+                    {
+                        myStrings[i] = dataGridViewStrings.Rows[i].Cells[1].Value?.ToString();
+                    }
+                    File.WriteAllLines(mySaveFileDialog.FileName, myStrings);
+                }
+            }
+        }
+
+        private void itemImport_Click(object sender, EventArgs e)
+        {
+            if (filename == "")
+                return;
+
+            using (OpenFileDialog myOpenFileDialog = new OpenFileDialog())
+            {
+                myOpenFileDialog.Filter = "Text file (*.txt) | *.txt";
+
+                if (myOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] myStrings = File.ReadAllLines(myOpenFileDialog.FileName);
+                    for (int i = 0; i < myStrings.Length; i++)
+                    {
+                        dataGridViewStrings.Rows[i].Cells[1].Value = myStrings[i];
+                    }
+                }
+            }
+        }
+    }
+}
