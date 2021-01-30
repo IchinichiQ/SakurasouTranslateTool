70a456567da954e4ea7983ff7dbf19726d624d39
12135555 <PechenkinPavelV@yandex.ru>
2021-01-30 22:56:55 +0300

    Add exception handling

--- SakurasouTranslateTool/Form1.cs
+++ SakurasouTranslateTool/Form1.cs
@@ -22,7 +22,7 @@ namespace SakurasouTranslateTool
             typeof(DataGridView).InvokeMember("DoubleBuffered",
             BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
             null, dataGridViewStrings, new object[] { true });
-            
+
             dataTable.Columns.Add("Pointer offset", typeof(int));
             dataTable.Columns.Add("Text", typeof(string));
 
@@ -39,147 +39,175 @@ namespace SakurasouTranslateTool
 
         private void buttonExtract_Click(object sender, EventArgs e)
         {
-            OpenFileDialog myDialog = new OpenFileDialog();
-
-            myDialog.Filter = "Ssb files (*.ssb)|*.ssb|All files (*.*)|*.*";
-            myDialog.RestoreDirectory = true;
-
-            if (myDialog.ShowDialog() == DialogResult.OK)
-                filename = myDialog.FileName;
-            else
-                return;
-
-            labelCurrentFile.Text = "Loading...";
-            labelCurrentFile.Update();
-
-            using (BinaryReader myReader = new BinaryReader(File.Open(filename, FileMode.Open)))
+            try
             {
-                int textOffset = -1;
-                for (int i = 0; i < myReader.BaseStream.Length; i += 30)
-                {
-                    myReader.BaseStream.Seek(i, SeekOrigin.Begin);
-                    byte[] chunk = myReader.ReadBytes(5);
-                    byte[] sample = { 0x2D, 0x2D, 0x2D, 0x2D, 0x2D};
-                    if (chunk.SequenceEqual(sample))
-                    {
-                        textOffset = i;
-                        break;
-                    }                   
-                }
+                OpenFileDialog myDialog = new OpenFileDialog();
 
-                if (textOffset == -1)
-                {
-                    MessageBox.Show("Text not found!", "STT", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    labelCurrentFile.Text = "None";
+                myDialog.Filter = "Ssb files (*.ssb)|*.ssb|All files (*.*)|*.*";
+                myDialog.RestoreDirectory = true;
+
+                if (myDialog.ShowDialog() == DialogResult.OK)
+                    filename = myDialog.FileName;
+                else
                     return;
-                }
 
-                dataTable.Rows.Clear();
-                dataTable.BeginLoadData();
+                labelCurrentFile.Text = "Loading...";
+                labelCurrentFile.Update();
 
-                for (int i = 0; i < textOffset; i += 4)
+                using (BinaryReader myReader = new BinaryReader(File.Open(filename, FileMode.Open)))
                 {
-                    myReader.BaseStream.Seek(i, SeekOrigin.Begin);
-                    byte[] chunk = myReader.ReadBytes(4);
-                    int pointer = BitConverter.ToInt32(chunk, 0) * 4;
-
-                    List<byte> stringBytes = new List<byte>();
-                    if (pointer > textOffset)
+                    int textOffset = -1;
+                    for (int i = 0; i < myReader.BaseStream.Length; i += 30)
                     {
-                        for (int ii = pointer; ii < myReader.BaseStream.Length; ii++)
+                        myReader.BaseStream.Seek(i, SeekOrigin.Begin);
+                        byte[] chunk = myReader.ReadBytes(5);
+                        byte[] sample = { 0x2D, 0x2D, 0x2D, 0x2D, 0x2D };
+                        if (chunk.SequenceEqual(sample))
                         {
-                            myReader.BaseStream.Seek(ii, SeekOrigin.Begin);
-                            byte charByte = myReader.ReadByte();
-                            if (charByte == 0x00)
-                                break;
-                            stringBytes.Add(charByte);
-                        }
-
-                        if (stringBytes.Count == 0)
-                            continue;
-
-                        string myString = Encoding.UTF8.GetString(stringBytes.ToArray());
-                        if (myString.StartsWith(" START : ") | myString.StartsWith("V_"))
-                            continue;
-                        if (myString.StartsWith(" END   : ") | myString.StartsWith("\n★★★\n★★★"))
+                            textOffset = i;
                             break;
+                        }
+                    }
 
-                        dataTable.Rows.Add(i, myString.Replace("\n","__"));
-                    }                 
+                    if (textOffset == -1)
+                    {
+                        MessageBox.Show("Text not found!", "STT", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        labelCurrentFile.Text = "None";
+                        return;
+                    }
+
+                    dataTable.Rows.Clear();
+                    dataTable.BeginLoadData();
+
+                    for (int i = 0; i < textOffset; i += 4)
+                    {
+                        myReader.BaseStream.Seek(i, SeekOrigin.Begin);
+                        byte[] chunk = myReader.ReadBytes(4);
+                        int pointer = BitConverter.ToInt32(chunk, 0) * 4;
+
+                        List<byte> stringBytes = new List<byte>();
+                        if (pointer > textOffset)
+                        {
+                            for (int ii = pointer; ii < myReader.BaseStream.Length; ii++)
+                            {
+                                myReader.BaseStream.Seek(ii, SeekOrigin.Begin);
+                                byte charByte = myReader.ReadByte();
+                                if (charByte == 0x00)
+                                    break;
+                                stringBytes.Add(charByte);
+                            }
+
+                            if (stringBytes.Count == 0)
+                                continue;
+
+                            string myString = Encoding.UTF8.GetString(stringBytes.ToArray());
+                            if (myString.StartsWith(" START : ") | myString.StartsWith("V_"))
+                                continue;
+                            if (myString.StartsWith(" END   : ") | myString.StartsWith("\n★★★\n★★★"))
+                                break;
+
+                            dataTable.Rows.Add(i, myString.Replace("\n", "__"));
+                        }
+                    }
                 }
-            }
 
-            dataTable.EndLoadData();
-            labelCurrentFile.Text = Path.GetFileName(filename);
-            buttonPatch.Enabled = true;
+                dataTable.EndLoadData();
+                labelCurrentFile.Text = Path.GetFileName(filename);
+                buttonPatch.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!" + Environment.NewLine + ex.ToString(), "SakurasouTranslateTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void buttonPatch_Click(object sender, EventArgs e)
         {
-            if (filename == "")
-                return;
-
-            using (BinaryWriter myWriter = new BinaryWriter(File.Open(filename, FileMode.Open)))
+            try
             {
-                for (int i = 0; i < dataTable.Rows.Count; i++)
+                if (filename == "")
+                    return;
+
+                using (BinaryWriter myWriter = new BinaryWriter(File.Open(filename, FileMode.Open)))
                 {
-                    int pointerOffset = (int)dataTable.Rows[i][0];
-                    byte[] newPointer = BitConverter.GetBytes((UInt32)(myWriter.BaseStream.Length / 4));
-                    myWriter.Seek(pointerOffset, SeekOrigin.Begin);
-                    myWriter.Write(newPointer, 0, newPointer.Length);
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        int pointerOffset = (int)dataTable.Rows[i][0];
+                        byte[] newPointer = BitConverter.GetBytes((UInt32)(myWriter.BaseStream.Length / 4));
+                        myWriter.Seek(pointerOffset, SeekOrigin.Begin);
+                        myWriter.Write(newPointer, 0, newPointer.Length);
 
-                    string newText = (string)dataTable.Rows[i][1];
-                    newText = newText.Replace("__", "\n");
-                    myWriter.Seek(0, SeekOrigin.End);
-                    myWriter.Write(Encoding.UTF8.GetBytes(newText));
+                        string newText = (string)dataTable.Rows[i][1];
+                        newText = newText.Replace("__", "\n");
+                        myWriter.Seek(0, SeekOrigin.End);
+                        myWriter.Write(Encoding.UTF8.GetBytes(newText));
 
-                    int nullCount = 4 - ((int)myWriter.BaseStream.Length % 4);
-                    byte[] nulls = new byte[] {0x00, 0x00, 0x00, 0x00 };
-                    myWriter.Write(nulls, 0, nullCount);
+                        int nullCount = 4 - ((int)myWriter.BaseStream.Length % 4);
+                        byte[] nulls = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        myWriter.Write(nulls, 0, nullCount);
+                    }
+                    myWriter.Flush();
                 }
-                myWriter.Flush();
+                MessageBox.Show("Done", "STT", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            MessageBox.Show("Done", "STT", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!" + Environment.NewLine + ex.ToString(), "SakurasouTranslateTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void itemExport_Click(object sender, EventArgs e)
         {
-            if (filename == "")
-                return;
-
-            using (SaveFileDialog mySaveFileDialog = new SaveFileDialog())
+            try
             {
-                mySaveFileDialog.Filter = "Text file (*.txt) | *.txt";
+                if (filename == "")
+                    return;
 
-                if (mySaveFileDialog.ShowDialog() == DialogResult.OK)
+                using (SaveFileDialog mySaveFileDialog = new SaveFileDialog())
                 {
-                    string[] myStrings = new string[dataTable.Rows.Count];
-                    for (int i = 0; i < myStrings.Length; i++)
+                    mySaveFileDialog.Filter = "Text file (*.txt) | *.txt";
+
+                    if (mySaveFileDialog.ShowDialog() == DialogResult.OK)
                     {
-                        myStrings[i] = (string)dataTable.Rows[i][1];
+                        string[] myStrings = new string[dataTable.Rows.Count];
+                        for (int i = 0; i < myStrings.Length; i++)
+                        {
+                            myStrings[i] = (string)dataTable.Rows[i][1];
+                        }
+                        File.WriteAllLines(mySaveFileDialog.FileName, myStrings);
                     }
-                    File.WriteAllLines(mySaveFileDialog.FileName, myStrings);
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!" + Environment.NewLine + ex.ToString(), "SakurasouTranslateTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
         private void itemImport_Click(object sender, EventArgs e)
         {
-            if (filename == "")
-                return;
-
-            using (OpenFileDialog myOpenFileDialog = new OpenFileDialog())
+            try
             {
-                myOpenFileDialog.Filter = "Text file (*.txt) | *.txt";
+                if (filename == "")
+                    return;
 
-                if (myOpenFileDialog.ShowDialog() == DialogResult.OK)
+                using (OpenFileDialog myOpenFileDialog = new OpenFileDialog())
                 {
-                    string[] myStrings = File.ReadAllLines(myOpenFileDialog.FileName);
-                    for (int i = 0; i < myStrings.Length; i++)
+                    myOpenFileDialog.Filter = "Text file (*.txt) | *.txt";
+
+                    if (myOpenFileDialog.ShowDialog() == DialogResult.OK)
                     {
-                        dataTable.Rows[i][1] = myStrings[i];
+                        string[] myStrings = File.ReadAllLines(myOpenFileDialog.FileName);
+                        for (int i = 0; i < myStrings.Length; i++)
+                        {
+                            dataTable.Rows[i][1] = myStrings[i];
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!" + Environment.NewLine + ex.ToString(), "SakurasouTranslateTool", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
